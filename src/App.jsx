a1270618b930e79a71e1a4aa8cabc9678e4c4d4f
Johan Lopez <johan.lopez@bitechconsulting.co>
2024-10-11 07:38:18 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ApiCoffeeCarouselBasic } from './components/ApiCoffeeCarouselBasic'
+
+function App() {
+
+  return (
+   <div>
+      <ApiCoffeeCarouselBasic />
+   </div>
+  )
+}
+
+export default App
